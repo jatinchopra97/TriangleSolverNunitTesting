96c7df5c55508d5f888f85f1e7bd0bdd2d57909c
Jatin Chopra <Jchopra0894@conestogac.on.ca>
2023-06-04 02:58:43 -0400

    Second Commit

--- TriangleSolver.nunitTests/UnitTest1.cs
+++ TriangleSolver.nunitTests/UnitTest1.cs
@@ -2,14 +2,26 @@
 
 public class Tests
 {
+    private  Triangle triangleSolver{ get; set; } = null!;
+
     [SetUp]
     public void Setup()
     {
+        triangleSolver = new Triangle();
     }
 
     [Test]
-    public void Test1()
+    public void AnalyzeTriangle_Input20and20and20_OutputvalidEquilateralTriangle()
     {
-        Assert.Pass();
+        //Arrange
+        int firstSide = 20;
+        int secondSide = 20;
+        int thirdSide = 20;
+
+        string expected = "The triangle is valid and is an EQUILATERAL";
+        //Act
+        string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+        //Assert
+        Assert.AreEqual(expected, actual);
     }
 }
