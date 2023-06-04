05ffe67fd82a1a8e09b9e1ecbbf6068050034854
Jatin Chopra <Jchopra0894@conestogac.on.ca>
2023-06-04 03:06:55 -0400

    second commit

--- TriangleSolver.nunitTests/UnitTest1.cs
+++ TriangleSolver.nunitTests/UnitTest1.cs
@@ -24,4 +24,47 @@ public class Tests
         //Assert
         Assert.AreEqual(expected, actual);
     }
+
+    [Test]
+    public void AnalyzeTriangle_Input20and20and10_OutputvalidIsoscelesTriangle()
+    {
+        //Arrange
+        int firstSide = 20;
+        int secondSide = 20;
+        int thirdSide = 10;
+
+        string expected = "The triangle is valid and is an ISOSCELES";
+        //Act
+        string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+        //Assert
+        Assert.AreEqual(expected, actual);
+    }
+    [Test]
+    public void AnalyzeTriangle_Input40and20and40_OutputvalidIsoscelesTriangle()
+    {
+        //Arrange
+        int firstSide = 40;
+        int secondSide = 20;
+        int thirdSide = 40;
+
+        string expected = "The triangle is valid and is an ISOSCELES";
+        //Act
+        string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+        //Assert
+        Assert.AreEqual(expected, actual);
+    }
+    [Test]
+    public void AnalyzeTriangle_Input60and20and60_OutputvalidIsoscelesTriangle()
+    {
+        //Arrange
+        int firstSide = 60;
+        int secondSide = 20;
+        int thirdSide = 60;
+
+        string expected = "The triangle is valid and is an ISOSCELES";
+        //Act
+        string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+        //Assert
+        Assert.AreEqual(expected, actual);
+    }
 }
