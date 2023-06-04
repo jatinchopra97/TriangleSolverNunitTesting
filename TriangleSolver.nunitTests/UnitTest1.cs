77eb7b22a6acb1c3402b900a330909e263ad9887
Jatin Chopra <Jchopra0894@conestogac.on.ca>
2023-06-04 02:44:00 -0400

    first commit

---
+++ TriangleSolver.nunitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace TriangleSolver.nunitTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ TriangleSolver/Program.cs
@@ -0,0 +1,12 @@
+﻿using TriangleSolver;
+// Get percentage
+var triangleCheck = new Triangle();
+Console.Write("Enter the firstSide: ");
+var firstSide = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the secondSide: ");
+var secondSide = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the thirdSide: ");
+var thirdSide = Convert.ToInt32(Console.ReadLine());
+var output = Triangle.AnalyzeTriangle(firstSide,secondSide,thirdSide);
+// Print grade
+Console.WriteLine($"output : {output} ");
